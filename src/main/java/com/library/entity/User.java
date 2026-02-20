b87ac405656840833f172b0438d3e6e00f7d76c1
A00335641: Eoin Concannon <A00335641@student.tus.ie>
2026-02-20 22:55:58 +0000

    updated configs, added user and userRepository

---
+++ src/main/java/com/library/entity/User.java
@@ -0,0 +1,30 @@
+package com.library.entity;
+
+import com.library.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // stored hashed
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
