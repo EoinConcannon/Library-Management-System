fa82e6cda29adb2dc471702da91bc553f8d9a617
A00335641: Eoin Concannon <A00335641@student.tus.ie>
2026-02-21 15:35:21 +0000

    Added two html files (index and account creation), added securityConfig

---
+++ src/main/java/com/library/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	// temporarly disabling the login until user story 2
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+		return http.build();
+	}
+}
