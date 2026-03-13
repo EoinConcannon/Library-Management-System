c53375fc9cc53502ec57e672f066ff512e4ebdf3
A00335641: Eoin Concannon <A00335641@student.tus.ie>
2026-03-13 17:10:30 +0000

    fixed issue with activeProfile causing integration tests to not work

--- src/test/java/com/library/BaseIntegrationTest.java
+++ src/test/java/com/library/BaseIntegrationTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "spring.profiles.active=test")
 public abstract class BaseIntegrationTest {
 
 	@LocalServerPort
@@ -14,13 +14,18 @@ public abstract class BaseIntegrationTest {
 	@BeforeEach
 	void setUp() {
 		RestAssured.port = port;
+		RestAssured.baseURI = "http://localhost";
 	}
 
 	// Helper to get a librarian token for protected endpoint tests
 	protected String getLibrarianToken() {
-		return io.restassured.RestAssured.given().contentType("application/json")
-				.body("{\"email\":\"test@librarian.test\",\"password\":\"test\"}").when().post("/api/auth/login").then()
-				.statusCode(200).extract().path("token");
+		io.restassured.response.Response response = RestAssured.given().contentType("application/json")
+				.body("{\"email\":\"test@librarian.test\",\"password\":\"test\"}").when().post("/api/auth/login");
+
+		System.out.println("LOGIN RESPONSE STATUS: " + response.getStatusCode());
+		System.out.println("LOGIN RESPONSE BODY: " + response.getBody().asString());
+
+		return response.then().statusCode(200).extract().path("token");
 	}
 
 	protected String getStudentToken() {
